426c22140ad3874a0ba6abe9041c2cca64b66355
Evgeny <16515461+evgn125@users.noreply.github.com>
2017-07-31 16:22:27 +0500

    optimization

--- gruntfile.js
+++ gruntfile.js
@@ -23,6 +23,47 @@ module.exports = function (grunt) {
         src: "css/style.css"
       }
     },
+    imagemin: {
+      options: {
+        optimizationLevel: 7,
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'img/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'build/img/'
+        }]
+      }
+    },
+    concat: {
+      dist: {
+        files: {
+          "build/script.js": ["js/jquery.min.js", "js/bootstrap.min.js", "js/script.js"]
+        }
+      }
+    },
+    uncss: {
+      dist: {
+        files: {
+          "build/style.css": ["index.html"]
+        }
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          "js/script.min.js": ["build/script.js"]
+        }
+      }
+    },
+    cssmin: {
+      dist: {
+        files: {
+          "css/style.min.css": ["build/style.css"]
+        }
+      }
+    },
     watch: {
       styles: {
         files: ["less/**/*.less"],
@@ -36,5 +77,15 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks("grunt-contrib-less");
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-postcss");
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+
+  grunt.loadNpmTasks('grunt-uncss');
+
+
+  grunt.registerTask("img", ["imagemin"]);
+  grunt.registerTask("min", ["concat", "uncss", "uglify", "cssmin"]);
   grunt.registerTask("default", ["watch"]);
 };
